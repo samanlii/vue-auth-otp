f9e981f41c8c43e4cdbf80a09935928fb94efe5a
Saman Torabli <torablisg@gmail.com>
2025-09-25 13:47:29 +0330

    feat: create RegisterPage.vue

---
+++ src/pages/RegisterPage.vue
@@ -0,0 +1,50 @@
+<template>
+  <q-page class="flex flex-center q-pa-md">
+    <q-form @submit.prevent="register">
+      <div class="q-pa-xl column items-center shadow-6 border">
+        <q-input
+          prefix="+98"
+          outlined
+          v-model="phone"
+          label="شماره تماس"
+          :error="!!errorMessage"
+          :error-message="errorMessage"
+        />
+        <q-btn
+          type="submit"
+          label="دریافت کد"
+          color="primary"
+          padding="xs lg"
+          class="full-width q-ma-md"
+        />
+      </div>
+    </q-form>
+  </q-page>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useAuthStore } from '../stores/auth'
+
+const phone = ref('')
+const authStore = useAuthStore()
+const errorMessage = ref('')
+
+function iranValidPhone(num) {
+  if (!num) return false
+  return /^09\d{9}$/.test(num) || /^9\d{9}$/.test(num)
+}
+function register() {
+  if (!iranValidPhone(phone.value)) {
+    errorMessage.value = 'شماره معتبر نیست'
+    return
+  }
+
+  errorMessage.value = ''
+  const otp = Math.floor(10000 + Math.random() * 90000)
+  console.log('کد ارسال شده به شماره', phone.value, ':', otp)
+
+  authStore.setPhone(phone.value)
+  authStore.setOtp(otp)
+}
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -2,14 +2,14 @@ const routes = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [{ path: '', component: () => import('pages/RegisterPage.vue') }],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue'),
+    component: () => import('src/pages/ErrorNotFound.vue'),
   },
 ]
 
