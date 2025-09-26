4a9f0e685770ad15d7fad2c8ce3a97283647654e
Saman Torabli <torablisg@gmail.com>
2025-09-26 14:29:51 +0330

    style: otp code input

--- src/pages/VerificationPage.vue
+++ src/pages/VerificationPage.vue
@@ -1,16 +1,23 @@
 <template>
   <q-page class="flex flex-center q-pa-md">
-    <q-form v-model="modelValue" @submit.prevent="verifyOtp">
+    <q-form @submit.prevent="verifyOtp">
       <div class="q-pa-xl column items-center shadow-6 border">
         <div class="q-mb-md">کد ارسال شده به {{ authStore.phone }} را وارد کنید</div>
-        <q-input
-          v-model="otpCode"
-          label="کد را وارد کنید"
-          maxlength="5"
-          outlined
-          :error="!!errorMessage"
-          :error-message="errorMessage"
-        />
+        <div class="row q-gutter-sm">
+          <q-input
+            v-for="(digit, index) in 5"
+            :key="index"
+            ref="inputs"
+            v-model="otp[index]"
+            maxlength="1"
+            @keyup="focusNext(index, $event)"
+            outlined
+            class="otp-box"
+          />
+        </div>
+        <div v-if="errorMessage" class="text-negative q-mt-sm">
+          {{ errorMessage }}
+        </div>
         <q-btn
           type="submit"
           label="تایید"
@@ -27,13 +34,22 @@
 import { ref } from 'vue'
 import { useAuthStore } from '../stores/auth'
 
-const modelValue = defineModel()
-const otpCode = ref('')
+const otp = ref(['', '', '', '', ''])
+const inputs = ref([])
 const errorMessage = ref('')
 const authStore = useAuthStore()
 
+function focusNext(index, event) {
+  const key = event.key
+  if (/^\d$/.test(key) && index < 4) {
+    inputs.value[index + 1].focus()
+  } else if (key === 'Backspace' && index > 0 && !otp.value[index]) {
+    inputs.value[index - 1].focus()
+  }
+}
+
 function verifyOtp() {
-  if (otpCode.value !== String(authStore.otp)) {
+  if (otp.value !== String(authStore.otp)) {
     errorMessage.value = 'کد اشتباه است'
     return
   }
@@ -42,3 +58,11 @@ function verifyOtp() {
   alert('ورود موفقیت‌آمیز ✅')
 }
 </script>
+
+<style scoped>
+.otp-box {
+  width: 50px;
+  text-align: center;
+  font-size: 20px;
+}
+</style>
